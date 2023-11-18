a0d5d26d6737ad084c846605945cc4a7d84b14b2
Andrzej Martyna <4319685+andrzejmartyna@users.noreply.github.com>
2023-11-18 20:10:43 +0100

    initial version - reads secret names

---
+++ kcs-cache/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+Console.WriteLine("Hello, World!");
+
+var azureKeyVaultName = "testy-kv";
+var secretClient = new SecretClient(new Uri($"https://{azureKeyVaultName}.vault.azure.net/"), new DefaultAzureCredential());
+foreach (var page in secretClient.GetPropertiesOfSecrets().AsPages())
+{
+    foreach (var s in page.Values)
+    {
+        Console.WriteLine(s.Name);
+    }
+}
