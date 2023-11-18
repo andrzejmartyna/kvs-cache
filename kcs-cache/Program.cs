0f8939c221134a55436381418cb3d5f3c887fc08
Andrzej Martyna <4319685+andrzejmartyna@users.noreply.github.com>
2023-11-18 20:10:43 +0100

    iterate over all secrets in all KVs in all subscriptions available

--- kcs-cache/Program.cs
+++ kcs-cache/Program.cs
@@ -1,16 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Azure.Identity;
+using Azure.ResourceManager;
 using Azure.Security.KeyVault.Secrets;
 
-Console.WriteLine("Hello, World!");
+var creds = new DefaultAzureCredential();
+var client = new ArmClient(creds);
 
-var azureKeyVaultName = "testy-kv";
-var secretClient = new SecretClient(new Uri($"https://{azureKeyVaultName}.vault.azure.net/"), new DefaultAzureCredential());
-foreach (var page in secretClient.GetPropertiesOfSecrets().AsPages())
+foreach (var subscription in client.GetSubscriptions())
 {
-    foreach (var s in page.Values)
+    Console.WriteLine($"Found subscription: {subscription.Data.DisplayName}");
+    foreach (var obj in subscription.GetGenericResources("resourceType eq 'Microsoft.KeyVault/vaults'"))
     {
-        Console.WriteLine(s.Name);
+        var azureKeyVaultName = obj.Data.Name;
+        Console.WriteLine($"Found KV: {azureKeyVaultName}");
+        var secretClient = new SecretClient(new Uri($"https://{azureKeyVaultName}.vault.azure.net/"), creds);
+        foreach (var page in secretClient.GetPropertiesOfSecrets().AsPages())
+        {
+            foreach (var s in page.Values)
+            {
+                Console.WriteLine($"Found secret: {s.Name} with value {secretClient.GetSecret(s.Name).Value.Value}");
+            }
+        }
     }
 }
